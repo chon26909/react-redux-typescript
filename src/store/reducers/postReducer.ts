1ae36566977f9603b36716571617705daa1243b8
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-05 14:10:59 +0700

    create post reducer

---
+++ src/store/reducers/postReducer.ts
@@ -0,0 +1,40 @@
+import Post from "../../models/postModel"
+import { Action } from "../action"
+import ActionType from "../action/action-types";
+
+interface PostState {
+    loading: boolean;
+    error: string | null;
+    data: Post[] | null
+}
+
+const initialState = {
+    loading: false,
+    error: null,
+    data: null
+}
+
+export const postReducer = (state: PostState = initialState, action: Action): PostState => {
+    switch (action.type) {
+        case ActionType.GET_POST_REQUEST:
+            return {
+                loading: true,
+                error: null,
+                data: null
+            }
+        case ActionType.GET_POST_SUCCESS:
+            return {
+                loading: false,
+                error: null,
+                data: action.payload
+            }
+        case ActionType.GET_POST_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+                data: null
+            }
+        default:
+            return state;
+    }
+}
